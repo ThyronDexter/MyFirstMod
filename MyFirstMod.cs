301aceefa345ba75c7cff8c426a722581429cd9c
Kirill <70391542+ThyronDexter@users.noreply.github.com>
2021-01-14 20:01:26 +0700

    Add files via upload

---
+++ MyFirstMod.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Trainworks;
+using Trainworks.Interfaces;
+using Trainworks.Managers;
+using System;
+
+namespace MyFirstMod
+{
+    [BepInPlugin("mod.myfirstmod", "My First Mod", "1.0.0")]
+    [BepInProcess("MonsterTrain.exe")]
+    [BepInProcess("MtLinkHandler.exe")]
+    public class MyFirstMod : BaseUnityPlugin, IInitializable
+    {
+        public void Initialize()
+        {
+            StygianDamageSpell.Create();
+        }
+        void Awake()
+        {
+            var harmony = new Harmony("mod.myfirstmod");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(SaveManager), "SetupRun")]
+        class AddCardToStartingDeckPatch
+        {
+            static void Postfix(ref SaveManager __instance)
+            {
+                __instance.AddCardToDeck(CustomCardManager.GetCardDataByID("Stygian_BitterCold"));
+            }
+        }
+    }
+}
---
+++ StygianDamageSpell.cs
@@ -0,0 +1,50 @@
+﻿using Trainworks.Builders;
+using Trainworks.Constants;
+using System.Collections.Generic;
+
+namespace MyFirstMod
+{
+    class StygianDamageSpell
+    {
+        public static void Create()
+        {
+            new CardDataBuilder
+            {
+                Cost = 1,
+                Rarity = CollectableRarity.Common,
+                CardType = CardType.Spell,
+                CardID = "Stygian_BitterCold",
+                Name = "Bitter Cold",
+                Description = "Deal <b>{[effect0.power]}</b> damage to the front enemy unit.",
+                AssetPath = "bittercold.png",
+                ClanID = VanillaClanIDs.Stygian,
+
+                CardPoolIDs = new List<string> { VanillaCardPoolIDs.MegaPool },
+
+                TargetsRoom = true,
+                Targetless = false,
+                EffectBuilders = new List<CardEffectDataBuilder>
+                {
+                    new CardEffectDataBuilder
+                    {
+                        EffectStateType = typeof(CardEffectDamage),
+                        TargetMode = TargetMode.FrontInRoom,
+                        TargetTeamType = Team.Type.Heroes,
+                        ParamInt = 3,
+                    }
+                },
+                TraitBuilders = new List<CardTraitDataBuilder>
+                {
+                    new CardTraitDataBuilder
+                    {
+                        TraitStateType = typeof(CardTraitIgnoreArmor)
+                    },
+                    new CardTraitDataBuilder
+                    {
+                        TraitStateType = typeof(CardTraitIntrinsicState)
+                    }
+                }
+            }.BuildAndRegister();
+        }
+    }
+}
